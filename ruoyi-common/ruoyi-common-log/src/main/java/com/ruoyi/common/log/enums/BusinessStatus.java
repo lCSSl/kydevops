5e5339c730ae1b177c3c4bb81702c6086edd7ff5
Hamster <chensuisheng@dasurebao.com>
2021-02-20 12:30:51 +0800

    ?

---
+++ ruoyi-common/ruoyi-common-log/src/main/java/com/ruoyi/common/log/enums/BusinessStatus.java
@@ -0,0 +1,17 @@
+package com.ruoyi.common.log.enums;
+
+/**
+ * 操作状态
+ *
+ * @author css
+ */
+public enum BusinessStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+    /**
+     * 失败
+     */
+    FAIL,
+}
